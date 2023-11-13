e22a3b4de64f6133177e5767cb688153aca20cd0
Salman Burhan <salmanfburhan@gmail.com>
2023-11-12 22:38:38 -0800

    Initial Commit

---
+++ ParkCSUSM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParkCSUSM
+//
+//  Created by Salman Burhan on 11/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParkCSUSM/ParkCSUSMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkCSUSMApp.swift
+//  ParkCSUSM
+//
+//  Created by Salman Burhan on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ParkCSUSMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
